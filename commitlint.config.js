a0b559b515a4eb82099fb5bcb418dd3f03a93035
bonabrian <bonabrian@investree.id>
2023-01-21 00:39:14 +0700

    chore: add git hooks with husky

---
+++ commitlint.config.js
@@ -0,0 +1,17 @@
+/**
+ * build: changes that affect the build system or external dependencies
+ * ci: changes to CI configuration files and script
+ * docs: documentation only changes
+ * feat: a new feature
+ * fix: a bug fix
+ * perf: a code change that improves performance
+ * refactor: a code change that neither fixes a bug nor adds a feature
+ * static: add or update static files
+ * style: changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
+ * test: adding missing tests or correcting existing tests
+ * chore: updating grunt tasks etc; no production code change
+ */
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+}
